162de82a67d22b6706b5c9c315454e6b726a9588
faiaz000 <faiaz000@hotmail.com>
2019-11-13 18:54:19 +0900

    authentication and authorization completed

---
+++ routes/user.js
@@ -0,0 +1,72 @@
+const express = require("express");
+
+const User = require("../models/user");
+
+const router = express.Router();
+
+const bcrypt = require("bcrypt");
+
+const jwt = require('jsonwebtoken');
+
+
+router.post("/signup", (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash
+    });
+    user
+      .save()
+      .then(result => {
+        res.status(201).json({
+          message: "User created!",
+          result: result
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
+});
+
+router.post("/login", (req, res, next) => {
+ let fetchedUser;
+  User.findOne({ email: req.body.email }).then(user => {
+    if (!user) {
+      return res.status(400).json({
+        message: "User not exists. Please register"
+      });
+    }
+    fetchedUser= user;
+    return bcrypt.compare(req.body.password,user.password)
+  })
+  .then((result)=>{
+    
+    if(!result){
+        return res.status(401).json({
+            message: "Authentication failed"
+        });
+    }
+    const token = jwt.sign({email:fetchedUser.email,userId:fetchedUser._id},'sercret_this_should_be_longer',{
+        expiresIn:'1h'
+    })
+
+    res.status(200).json({
+        message:'authentication successful',
+        token:token,
+        expiresIn : 3600,
+        userId: fetchedUser._id
+    });
+  })
+  .catch((err)=>{
+     
+    return res.status(401).json({
+        message: "Authentication failed"
+      });
+  });
+});
+
+module.exports = router;
